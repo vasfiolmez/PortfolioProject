78029cae47166bb0517839e12840fb0e2832827c
Vasfi Ölmez <58458944+vasfiolmez@users.noreply.github.com>
2024-09-19 22:10:57 +0300

    4.Ders Tamamlandı.

---
+++ MyPortfolioProject/Controllers/SkillController.cs
@@ -0,0 +1,44 @@
+﻿using MyPortfolioProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolioProject.Controllers
+{
+    public class SkillController : Controller
+    {
+        DbMyPortfolioEntities context = new DbMyPortfolioEntities();
+        public ActionResult SkillList()
+        {
+            var values = context.Skills.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult CreateSkill()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateSkill(Skills skills)
+        {
+            context.Skills.Add(skills);
+            context.SaveChanges();
+            return RedirectToAction("SkillList");
+        }
+        public ActionResult DeleteSkill(int id) 
+        {
+            var value = context.Skills.Find(id);
+            context.Skills.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("SkillList");
+        }
+        [HttpGet]
+        public ActionResult UpdateSkill(int id) 
+        {
+           var value=context.Skills.Find(id);
+            return View(value);
+        }     
+    }
+}
